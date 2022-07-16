0823c58517c5c7e9a50ad2ffa10f9a2aa405e5f6
Thail3 <tanai.saeng@gmail.com>
2022-07-16 16:49:09 +0700

    JWT authorization verification

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,33 @@
 import express from "express";
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from "../controllers/user.js";
+import { verifyAdmin, verifyUser, verityToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+// router.get("/checkauthentication", verityToken, (req, res, next) => {
+//   res.send("Hello User, You are logged in");
+// });
+
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//   res.send("Hello User, You are logged in and you can delete your account");
+// });
+
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//   res.send("Hello Admin, You are logged in and you can delete All account");
+// });
+
+//UPDATE
+router.put("/:id", verifyUser, updateUser);
+//DELETE
+router.delete("/:id", verifyUser, deleteUser);
+//GET
+router.get("/:id", verifyUser, getUser);
+//GET ALL
+router.get("/", verifyAdmin, getUsers);
 
 export default router;

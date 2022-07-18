970794e9122544e07569665bd20dec6cf2c6b9f4
Thail3 <tanai.saeng@gmail.com>
2022-07-18 13:26:45 +0700

    getHotels from controller can limit item for show and limit cheapestPrice

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -20,6 +20,7 @@ export const updateHotel = async (req, res, next) => {
       },
       { new: true }
     );
+
     res.status(201).json(updatedHotel);
   } catch (error) {
     next(error);
@@ -45,8 +46,13 @@ export const getHotel = async (req, res, next) => {
 };
 
 export const getHotels = async (req, res, next) => {
+  const { min, max, ...others } = req.query;
   try {
-    const hotels = await Hotel.find();
+    const hotels = await Hotel.find({
+      ...others,
+      cheapestPrice: { $gt: min | 1, $lt: max || 999 },
+    }).limit(req.query.limit);
+
     res.status(201).json(hotels);
   } catch (error) {
     next(error);
@@ -55,12 +61,14 @@ export const getHotels = async (req, res, next) => {
 
 export const countByCity = async (req, res, next) => {
   const cities = req.query.cities.split(",");
+
   try {
     const list = await Promise.all(
       cities.map((city) => {
         return Hotel.countDocuments({ city: city });
       })
     );
+
     res.status(200).json(list);
   } catch (err) {
     next(err);
